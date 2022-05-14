e082f9092c0e20369c8cecfedc2492b408539c90
Mert Even <me@merteven.com>
2022-05-14 20:20:20 +0300

    Initial commit

---
+++ src/main/java/meven/movie/collectionmanagement/MovieCollectionManagementApplication.java
@@ -0,0 +1,12 @@
+package meven.movie.collectionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCollectionManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovieCollectionManagementApplication.class, args);
+  }
+}
---
+++ src/test/java/meven/movie/collectionmanagement/MovieCollectionManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package meven.movie.collectionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCollectionManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
